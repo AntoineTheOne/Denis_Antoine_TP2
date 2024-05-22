8b9238c609cf1f3155e5d66dc138bbbc6160e00b
AntoineTheOne <Antoine.Denis2@outlook.com>
2024-05-21 23:09:22 -0400

    petite mise à jour de truc

--- Assets/Scripts/PriseSacDeGraine.cs
+++ Assets/Scripts/PriseSacDeGraine.cs
@@ -5,18 +5,18 @@ public class PriseSacDeGraine : MonoBehaviour
     [SerializeField] private GameObject graineDansMain;
     [SerializeField] private InfosJoueur infosJoueur;
 
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-
-
             graineDansMain.SetActive(true);
             Destroy(gameObject);
             infosJoueur.sacJaune = true;
             Debug.Log(infosJoueur.sacJaune);
         }
+        }
     }
-}
+
 
 // && gameObject.CompareTag("Jaune")

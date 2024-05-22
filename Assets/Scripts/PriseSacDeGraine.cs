45ae07ef06d2df6f7f63af88b319f271c0cfedda
AntoineTheOne <Antoine.Denis2@outlook.com>
2024-05-22 12:16:07 -0400

    ajout des script Planter arbre

--- Assets/Scripts/PriseSacDeGraine.cs
+++ Assets/Scripts/PriseSacDeGraine.cs
@@ -6,7 +6,7 @@ public class PriseSacDeGraine : MonoBehaviour
     [SerializeField] private GameObject graineDansMain;
     [SerializeField] private InfosJoueur infosJoueur;
 
-
+    
 
 void Start(){
     infosJoueur.sacEnMain = false;
@@ -18,8 +18,11 @@ void Start(){
             if (other.gameObject.CompareTag("Player") && infosJoueur.sacEnMain == false)
             {
                 infosJoueur.sacEnMain = true;
-                Debug.Log( "test: " + other.gameObject.transform.Find("AncorSac"));
-                transform.SetParent(other.gameObject.transform.Find("AncorSac"), false);
+                transform.SetParent(other.gameObject.transform.Find("AncorSac"), false); 
+                
+                //Vector3 positionSac = new Vector3(0f, 0f, 0f);
+               //transform.SetPositionAndRotation( positionSac, other.gameObject.transform.rotation);
+                transform.localPosition = new Vector3(0,0,0);
                 infosJoueur.sacJaune = true;
             }
         
